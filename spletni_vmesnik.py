a12e0e94689f2435669db2493a5cb2c5e2b2a401
ursakumeljfaks <105431237+ursakumeljfaks@users.noreply.github.com>
2024-03-01 16:52:25 +0100

    html za naslovno stran in za voznike + dodan razred uporabnik in za geslo

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,7 +1,40 @@
 import bottle
+from model import Voznik
 
 @bottle.get('/')
 def naslovna_stran():
-    return bottle.template('index.html')
+    return bottle.template('naslovna_stran.html', napaka=None)
+
+@bottle.get('/voznik/')
+def voznik():
+    return bottle.template('voznik.html', napaka=None, ime="")
+
+@bottle.post('/voznik/')
+def dodaj_voznika_post():
+    # zahtevaj_prijavo()
+    ime = bottle.request.forms.getunicode('ime')
+    if not ime[0].isupper():
+        return bottle.template(
+            'voznik.html',
+            napaka='Ime se mora začeti z veliko začetnico!',
+            ime=ime
+        )
+    else:
+        oseba = Voznik(ime)
+        oseba.dodaj_v_bazo()
+        bottle.redirect('/')
+
+@bottle.get('/voznik/')
+def isci():
+    iskalni_niz = bottle.request.query.getunicode('iskalni_niz')
+    osebe = Voznik.poisci(iskalni_niz)
+    return bottle.template(
+        'voznik.html',
+        iskalni_niz=iskalni_niz,
+        osebe=osebe
+    )
+
+
+
 
 bottle.run(debug=True, reloader=True)
