ae5f712fd4d902f1d33e4050daeb38aae531b16c
ursakumeljfaks <105431237+ursakumeljfaks@users.noreply.github.com>
2024-03-02 19:59:30 +0100

    stran za voznika dodana + spletni_vmesnik dopolnjen + popravljeno

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -6,34 +6,38 @@ def naslovna_stran():
     return bottle.template('naslovna_stran.html', napaka=None)
 
 @bottle.get('/voznik/')
-def voznik():
-    return bottle.template('voznik.html', napaka=None, ime="")
-
-@bottle.post('/voznik/')
-def dodaj_voznika_post():
-    # zahtevaj_prijavo()
-    ime = bottle.request.forms.getunicode('ime')
-    if not ime[0].isupper():
-        return bottle.template(
-            'voznik.html',
-            napaka='Ime se mora začeti z veliko začetnico!',
-            ime=ime
-        )
-    else:
-        oseba = Voznik(ime)
-        oseba.dodaj_v_bazo()
-        bottle.redirect('/')
-
-@bottle.get('/voznik/')
-def isci():
+def isci_voznika():
     iskalni_niz = bottle.request.query.getunicode('iskalni_niz')
-    osebe = Voznik.poisci(iskalni_niz)
+    vozniki = Voznik.poisci(iskalni_niz)
     return bottle.template(
         'voznik.html',
         iskalni_niz=iskalni_niz,
-        osebe=osebe
+        vozniki=vozniki
     )
 
+@bottle.get('/voznik/<priimek>/')
+def tocke_in_zmage_voznika(priimek):
+    vozniki = Voznik.poisci(priimek)
+    tocke = []
+    zmage = []
+    profil = []
+    ekipe = []
+    for voznik in vozniki:
+        tocke.extend(voznik.poisci_tocke())
+        zmage.extend(voznik.poisci_zmage())
+        profil.append((voznik.poisci_skupno_st_nastopov(), 
+                       voznik.poisci_skupno_st_zmag(), 
+                       voznik.poisci_skupno_st_stopnick(), 
+                       voznik.poisci_skupno_st_tock()))
+        ekipe.extend(voznik.poisci_ekipe())
+    return bottle.template(
+        'voznik_vse.html',
+        tocke=tocke,
+        voznik=voznik,
+        zmage=zmage,
+        profil=profil,
+        ekipe=ekipe
+    )
 
 
 
