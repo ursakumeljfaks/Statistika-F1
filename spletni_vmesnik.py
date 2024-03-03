8d0104e9b6acbfe3a57509e9ff6caf32ffd212f7
ursakumeljfaks <105431237+ursakumeljfaks@users.noreply.github.com>
2024-03-03 17:02:58 +0100

    eipa, dirka, proga

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,5 +1,5 @@
 import bottle
-from model import Voznik
+from model import Voznik, Ekipa, Proga, Dirka
 
 @bottle.get('/')
 def naslovna_stran():
@@ -39,6 +39,66 @@ def tocke_in_zmage_voznika(priimek):
         ekipe=ekipe
     )
 
+@bottle.get('/ekipa/')
+def isci_ekipo():
+    iskalni_niz = bottle.request.query.getunicode('iskalni_niz')
+    ekipe = Ekipa.poisci(iskalni_niz)
+    return bottle.template(
+        'ekipa.html',
+        iskalni_niz=iskalni_niz,
+        ekipe=ekipe
+    )
+
+@bottle.get('/ekipa/<ime>/')
+def voznik_in_leto_ekipe(ime):
+    ekipe = Ekipa.poisci(ime)
+    vozniki = []
+    for ekipa in ekipe:
+        vozniki.extend(ekipa.poisci_voznike())
+    return bottle.template(
+        'ekipa_vse.html',
+        vozniki=vozniki,
+        ekipa=ekipa
+    )
+
+@bottle.get('/proga/')
+def isci_progo():
+    iskalni_niz = bottle.request.query.getunicode('iskalni_niz')
+    proge = Proga.poisci(iskalni_niz)
+    return bottle.template(
+        'proga.html',
+        iskalni_niz=iskalni_niz,
+        proge=proge
+    )
+
+@bottle.get('/proga/<ime>/')
+def zmagovalci_proge(ime):
+    proge = Proga.poisci(ime)
+    zmagovalci = []
+    for proga in proge:
+        zmagovalci.extend(proga.poisci_zmagovalce())
+    return bottle.template(
+        'proga_vse.html',
+        zmagovalci=zmagovalci,
+        proga=proga
+    )
+
+@bottle.get('/dirka/')
+def isci_leta_dirka():
+    leta = Dirka.poisci_leto()
+    return bottle.template(
+        'dirka.html',
+        leta=leta
+    )
+
+@bottle.get('/dirka/<leto>/')
+def dirka_leto(leto):
+    dirke = Dirka.poisci_dirke_v_letu(leto)
+    return bottle.template(
+        'dirka_leto.html',
+        dirke=dirke,
+        leto=leto
+    )
 
 
 bottle.run(debug=True, reloader=True)
